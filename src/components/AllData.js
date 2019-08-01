05772dab3bb9204934cebe71b5a9328b52d921a8
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-07-31 17:11:22 -0700

    Alldata displaying

--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { tsConstructorType } from '@babel/types';
+
 
 //const API = 'http://watermyplant-backend-env.x589jebncj.us-east-1.elasticbeanstalk.com/data'
 const API = 'http://localhost:3000/auth/data';
@@ -9,27 +11,34 @@ export default function AllData() {
   //Set status
   const [readings, setReadings] = useState([]);
 
+  //Convert timestamp from epoch to readable date
+  const convert = (cur) => {
+    cur.timestamp = new Date(cur.timestamp * 1000);
+    return cur;
+  }
+
+  //get readings from the API
   const _getReadings = () => {
     
     // fetch from API
     fetch(API)
-    .then( data => data.json() )
-    .then( readingList => {
-      setReadings(readingList);
-      console.log(readingList); 
+    .then( data => data.json())
+    .then(readingList => {
+       setReadings(readingList);
     })
+
     .catch( console.error );
   };
 
   useEffect(_getReadings, []);
-
+  
   return (
     <>
     <p>Readings</p>
     <ul>
     {readings.map( (reading, index) =>
       <li key={index}>
-        <p>{reading.timestamp}</p>
+        <p>{convert(reading).timestamp.toString()}</p>
       </li>
     )}
   </ul>
