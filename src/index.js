737700ce340de03c4e55acc99ba85cd47fb746f6
chidrome <chidrome@gmail.com>
2019-08-01 19:02:24 -0700

    removing sass for deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './scss/core.scss';
+// import './scss/core.scss';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
